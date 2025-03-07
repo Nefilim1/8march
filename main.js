b4d0c2dc8c35f85e5cb0447d7e3127cf52d6b428
Nefilim1 <regent0933@gmail.com>
2025-03-07 14:48:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        icon: path.join(__dirname, 'icon.ico')
+    });
+
+    win.loadFile('index.html');
+    // Раскомментируйте следующую строку, чтобы открыть инструменты разработчика
+    // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+}); 
